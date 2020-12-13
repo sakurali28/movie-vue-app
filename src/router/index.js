f374a7b22c5d5a9aa82d744b8d926222ab55a400
YOUR NAME <YOUR@EMAIL.com>
2020-12-13 00:28:14 -0600

    adds authentication

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,47 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/logout",
+    name: "Logout",
+    component: Logout,
+  },
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,114 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome Back!</h1>
+
+    <h2>Create a New Movie</h2>
+    <div>
+      Title: <input type="text" v-model="newMovieTitle">
+      Year: <input type="text" v-model="newMovieYear">
+      Plot: <input type="text" v-model="newMoviePlot">
+      Director: <input type="text" v-model="newMovieDirector">
+    </div>
+    <button v-on:click="createMovie()">create</button>
+
+   <h2>Movies Index</h2>
+   <div v-for="movie in movies">
+     <h3>{{ movie.title }}</h3>
+     <p>{{ movie.year }}</p>
+     <button v-on:click="showMovie(movie)">more info</button>
+   </div>
+
+  <dialog id="movie-details">
+    <form method="dialog" >
+      <p>Title: <input type="text" v-model="currentMovie.title"></p>
+      <p>Year: <input type="text" v-model="currentMovie.year"></p>
+      <p>Plot: <input type="text" v-model="currentMovie.plot"></p>
+      <p>Director: <input type="text" v-model="currentMovie.director"></p>
+      <button v-on:click="updateMovie(currentMovie)">create</button>
+      <button v-on:click="destroyMovie(currentMovie)">delete</button>
+      <button>close</button>
+    </form>
+  </dialog>
+
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      movies: [],
+      newMovieTitle: "",
+      newMovieYear: "",
+      newMoviePlot: "",
+      newMovieDirector: "",
+      currentMovie: {},
+    };
+  },
+  
+  created: function() {
+    this.indexMovies();
+  },
+
+  methods: {
+    indexMovies: function () {
+      axios
+        .get("/api/movies")
+        .then(response => {
+          this.movies = response.data;
+          console.log("All Movies", this.movies);
+        });
+    },
+
+    createMovie: function() {
+      var params = {
+        title: this.newMovieTitle,
+        year: this.newMovieYear,
+        plot: this.newMoviePlot,
+        director: this.newMovieDirector,
+      };
+      axios
+        .post("/api/movies", params)
+        .then(response => {
+          console.log("created successfully!", response.data);
+          this.movies.push(response.data);
+        })
+        .catch(error => console.log(error.response));
+    },
+
+    showMovie: function(movie) {
+      this.currentMovie = movie;
+      document.querySelector("#movie-details").showModal();
+    },
+
+    updateMovie: function(movie) {
+      var params = {
+        title: movie.title,
+        year: movie.year,
+        plot: movie.plot,
+        director: movie.director,
+      };
+      axios
+        .patch("/api/movies/" + movie.id, params)
+        .then(response => {
+          console.log("successfully updated!", response.data);
+        });
+    },
+
+    destroyMovie: function(movie) {
+      axios
+        .delete("/api/movies/" + movie.id)
+        .then(response => {
+          console.log("deleted!", response.data);
+          var index = this.movies.indexOf(movie);
+          this.movies.splice(index, 1);
+        });
+    },
   }
-}
+};
 </script>
